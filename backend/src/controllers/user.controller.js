6018dcf1ecc87750db69e1439a650c8b800c7103
imvaibhav507 <64185828+imvaibhav507@users.noreply.github.com>
2024-03-10 20:33:21 +0530

    register user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,1 +1,61 @@
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import mongoose from "mongoose";
+import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 
+const registerUser = AsyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
+
+  console.log(fullName, email, username);
+
+  // validation
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existingUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existingUser) {
+    throw new ApiError(409, "User with given name or email already exist.");
+  }
+
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+  console.log(avatarLocalPath);
+
+  // upload to cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar not found");
+  }
+
+  // create new db entry
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while creating user");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+});
+
+export { registerUser };
