cc3e43ae1184ae302b4ee5ddd1a066bd76db814c
imvaibhav507 <64185828+imvaibhav507@users.noreply.github.com>
2024-04-08 19:12:04 +0530

    posts ui

--- frontend/lib/presentation/daily_new_page/widgets/post_item_widget.dart
+++ frontend/lib/presentation/daily_new_page/widgets/post_item_widget.dart
@@ -3,33 +3,30 @@ import '../controller/daily_new_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:vaibhav_s_application2/core/app_export.dart';
 
+import '../models/post_model.dart';
+
 // ignore: must_be_immutable
-class FortyItemWidget extends StatelessWidget {
-  FortyItemWidget(
-    this.fortyItemModelObj, {
+class PostItemWidget extends StatelessWidget {
+  PostItemWidget(
+    this.attachment, {
     Key? key,
   }) : super(
           key: key,
         );
 
-  FortyItemModel fortyItemModelObj;
+  String attachment;
 
   var controller = Get.find<DailyNewController>();
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 167.h,
-      child: Obx(
-        () => CustomImageView(
-          imagePath: fortyItemModelObj.forty!.value,
-          height: 167.adaptSize,
-          width: 167.adaptSize,
+      child: CustomImageView(
+          imagePath: attachment,
           radius: BorderRadius.circular(
-            10.h,
+            16.h,
           ),
         ),
-      ),
     );
   }
 }
